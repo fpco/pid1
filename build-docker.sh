dd8cb039a7f13030710bbc6e8ad724e917a1be7e
Michael Snoyman <michael@fpcomplete.com>
2016-11-23 05:58:43 +0000

    Build static executables

--- build-docker.sh
+++ build-docker.sh
@@ -2,9 +2,22 @@
 
 set -eux
 
+docker build --tag fpco/static-base static-base
+
+LAST_LINE=$(stack sdist 2>&1 | tail -n 1)
+SDIST=${LAST_LINE##* }
+
 rm -rf build-docker
-mkdir -p build-docker
-stack install --local-bin-path build-docker
+mkdir build-docker
+mkdir -p build-home
+docker run --rm \
+    -v $(pwd)/build-docker:/host-bin \
+    -v $SDIST:/sdist.tar.gz \
+    -v $(pwd)/build-home:/home/build \
+    fpco/static-base \
+    /bin/bash -c \
+    'tar zxfv /sdist.tar.gz && cd pid1-* && stack install --test --local-bin-path /host-bin --ghc-options "-optl-static -fPIC -optc-Os" && upx --best --ultra-brute /host-bin/pid1'
+
 
 cat > build-docker/Dockerfile <<EOF
 FROM ubuntu:16.04
