137875f2ffabedce32d0834e84d8c0107bdb34df
Michael Snoyman <michael@snoyman.com>
2016-11-23 07:16:35 +0200

    Turn off RTS options

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,3 +1,7 @@
+## 0.1.0.1
+
+* Turn off all RTS options
+
 ## 0.1.0.0
 
 * Initial release
--- build-docker.sh
+++ build-docker.sh
@@ -11,7 +11,7 @@ FROM ubuntu:16.04
 MAINTAINER Michael Snoyman (michael@fpcomplete.com)
 RUN apt-get update && apt-get install -y libgmp10
 ADD pid1 /sbin/pid1
-ENTRYPOINT ["/sbin/pid1"]
+ENTRYPOINT ["/sbin/pid1", "--RTS"]
 EOF
 
 docker build --tag fpco/pid1:16.04 build-docker
