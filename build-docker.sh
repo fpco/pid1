a132fda5b383b4cff5af7905a4d5c6315a877f1a
Sibi Prabakaran <sibi@psibi.in>
2022-11-28 16:54:10 +0530

    Update pid1 for latest lts
    The original motivation of the PR is to create a new image based on
    Ubuntu 22.04 as I want to use 22.04 as base in one of the project.
    
    Some comments on code changes:
    
    - Remove apt update step since it was causing around ~20 MB extra
    space. I believe it's better to the upgrade and other steps in the
    derived images instead.  - Do some modification to the bash code based
    on feedback from shellcheck.

--- build-docker.sh
+++ build-docker.sh
@@ -10,30 +10,29 @@ rm -rf build-docker
 mkdir build-docker
 mkdir -p build-home
 docker run --rm \
-    -v $(pwd)/build-docker:/host-bin \
-    -v $SDIST:/sdist.tar.gz \
-    -v $(pwd)/build-home:/home/build \
-    fpco/docker-static-haskell:8.0.2 \
+    -v "$(pwd)"/build-docker:/host-bin \
+    -v "$SDIST":/sdist.tar.gz \
+    -v "$(pwd)"/build-home:/home/build \
+    fpco/alpine-haskell-stack:9.2.5 \
     /bin/bash -c \
-    'chown $(id -u) $HOME && rm -rf $HOME/pid1-* && tar zxfv /sdist.tar.gz && cd pid1-* && stack install --system-ghc --test --local-bin-path /host-bin --ghc-options "-optl-static -fPIC -optc-Os" && upx --best --ultra-brute /host-bin/pid1'
+    'chown $(id -u) $HOME && rm -rf $HOME/pid1-* && tar zxfv /sdist.tar.gz && cd pid1-* && stack install --system-ghc --flag pid1:static --local-bin-path /host-bin'
 
 
 cat > build-docker/Dockerfile <<EOF
-FROM ubuntu:20.04
+FROM ubuntu:22.04
 MAINTAINER Michael Snoyman (michael@fpcomplete.com)
-RUN apt-get update && apt-get install -y libgmp10
 ADD pid1 /sbin/pid1
 ENTRYPOINT ["/sbin/pid1", "--RTS"]
 EOF
 
-docker build --tag fpco/pid1:20.04 build-docker
+docker build --tag fpco/pid1:22.04 build-docker
 
 # Sanity check
-docker run --rm fpco/pid1:20.04 ps
+docker run --rm fpco/pid1:22.04 ps
 
 # Push
-docker tag fpco/pid1:20.04 fpco/pid1:latest
-docker tag fpco/pid1:20.04 fpco/pid1:${VERSION}
-docker push fpco/pid1:20.04
-docker push fpco/pid1:${VERSION}
+docker tag fpco/pid1:22.04 fpco/pid1:22.04
+docker tag fpco/pid1:22.04 fpco/pid1:"${VERSION}"
+docker push fpco/pid1:22.04
+docker push fpco/pid1:"${VERSION}"
 docker push fpco/pid1:latest
